24dfcfc0bb0323cd1cc6e69ff0eaebf91b33b3f8
Mamoune <mathieu.noel@oathkeeper.xyz>
2021-09-29 14:05:34 +0200

    Suppression du fichier cpu_usage

--- cpu_usage.py
+++
@@ -1,9 +0,0 @@
-import os
-import psutil
-
-# Getting loadover15 minutes
-load1, load5, load15 = psutil.getloadavg()
-
-cpu_usage = (load15/os.cpu_count()) * 100
-
-print("The CPU usage is : ", cpu_usage, "%")
