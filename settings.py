54deee20a283da85e8e22f9de55179e8c334f28b
Mamoune <mathieu.noel@oathkeeper.xyz>
2021-09-29 14:07:50 +0200

    Ajout du fichier settings

---
+++ settings.py
@@ -0,0 +1,19 @@
+#Config : Différents paramètres du programme
+import os, psutil, socket
+
+def ip_and_host():
+	hostname = socket.gethostname()
+
+	print(f"Hostname: {hostname}")
+
+
+def cpu():
+	load1, load5, load15 = psutil.getloadavg()
+	cpu_usage = (load15/os.cpu_count()) * 100
+
+	print("The CPU usage is : ", cpu_usage, "%")
+
+
+def ram():
+	print('RAM memory % used:', psutil.virtual_memory()[2], "%")
+
