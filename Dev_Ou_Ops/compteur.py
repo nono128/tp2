ab3c12f14184eb994ec8d522c1f3f28c804d2f34
Mamoune <mathieu.noel@oathkeeper.xyz>
2021-10-11 11:58:09 +0200

    Ajout des fichiers compteur.py & test_compteur.py et du dossier logs

---
+++ Dev_Ou_Ops/compteur.py
@@ -0,0 +1,31 @@
+#import cgitb
+#cgitb.enable()
+
+file_compteur = "compteur"
+
+
+import os.path
+
+
+def lire_compteur():
+
+    if os.path.isfile(file_compteur):
+        print("Compteur existe") 
+        fichier = open(file_compteur,'r')
+#Lecture fichier compteur
+        nb_visiteurs = int(fichier.read())
+# "int" transforme le contenu en un nombre car on veut le nombre de visiteurs)
+
+    else:   
+        print("Compteur existe pas ")
+        nb_visiteurs = 0 
+#Le fichier n'existe pas / page jamais visitée
+        fichier = open(file_compteur,'w')
+
+    fichier = open(file_compteur,'w')
+    fichier.write(str(nb_visiteurs+ 1))
+    print(nb_visiteurs+1, 'visites')
+
+
+lire_compteur()
+
