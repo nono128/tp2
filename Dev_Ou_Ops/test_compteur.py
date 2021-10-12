ceb5c801523686f50efb1d12adb7bc9b6e16f30d
Mamoune <mathieu.noel@oathkeeper.xyz>
2021-10-12 14:42:01 +0200

    test_compteur

--- Dev_Ou_Ops/test_compteur.py
+++ Dev_Ou_Ops/test_compteur.py
@@ -1,16 +1,23 @@
 from bottle import route, run, template
 from compteur import *
-
+import os, socket, psutil
 
 @route('/')
-def count():
+def stat():
 
-	return "Nombre de visites : " + str(lire_compteur())
+    return "<b> Hostname : </b>" + str(socket.gethostname()) + "<br>" \
+           "<b> CPU is : </b>" + str(psutil.cpu_percent()) + "% <br>" \
+           "<b> Ram is : </b>" + str(psutil.virtual_memory().percent) + "% <br>" \
+	   "<b> Nombre de visites :</b>" + str(lire_compteur())
+
+
+
+
+#return "Nombre de visites : " + str(lire_compteur())
 
 
 
 run(host='0.0.0.0', port=8098)
-
 
 
 
